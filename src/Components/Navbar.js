66d8dc48eb44910162215dc1824cb9c19ae4de1b
Soumya Ganguly <kalpanaganguly.behala94@gmail.com>
2023-11-06 18:34:33 +0530

    navbar updated

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, {useEffect} from "react";
 // import PropTypes from 'prop-types'
-import { Link } from "react-router-dom";
+import { Link , useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  let location = useLocation();
+  useEffect(()=>{
+    console.log(location.pathname)
+  }, [location])
   return (
     <nav className="navbar navbar-expand-lg  navbar-dark bg-dark fixed-top">
       <div className="container-fluid">
@@ -23,42 +27,42 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">
+              <Link className={`nav-link ${location.pathname === "/" ? "active" : "" }`} aria-current="page" to="/">
                 Home
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/business">
+              <Link className={`nav-link ${location.pathname === "/business" ? "active" : "" }`} to="/business">
                 Business{" "}
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/entertainment">
+              <Link className={`nav-link ${location.pathname === "/entertainment" ? "active" : "" }`} to="/entertainment">
                 Entertainment
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/health">
+              <Link className={`nav-link ${location.pathname === "/health" ? "active" : "" }`} to="/health">
                 Health
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/science">
+              <Link className={`nav-link ${location.pathname === "/science" ? "active" : "" }`} to="/science">
                 Science
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/sports">
+              <Link className={`nav-link ${location.pathname === "/sports" ? "active" : "" }`} to="/sports">
                 Sports
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/technology">
+              <Link className={`nav-link ${location.pathname === "/technology" ? "active" : "" }`} to="/technology">
                 Technology{" "}
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about">
+              <Link className={`nav-link ${location.pathname === "/about" ? "active" : "" }`} to="/about">
                 About Us
               </Link>
             </li>
